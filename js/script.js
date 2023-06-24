487895207136064a973a43ebe53fb64c1448d814
João Félix <felixjoao651@gmail.com>
2023-06-23 20:56:48 -0400

    mobile e JS em produção

---
+++ js/script.js
@@ -0,0 +1,7 @@
+import navBar from './modules/scroll-navBar.js';
+import navBarScroll from './modules/btn-mobile.js';
+import initTabNav from './modules/initTabnav.js';
+
+navBar();
+navBarScroll();
+initTabNav();
