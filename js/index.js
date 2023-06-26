c8b9cdea3f13aa79d6e0eec43c8a06f1fc9c5e67
João Félix <felixjoao651@gmail.com>
2023-06-26 13:13:05 -0400

    projeto finalizado

--- js/index.js
+++ js/index.js
@@ -1,7 +1,5 @@
 import navBar from './modules/scroll-navBar.js';
 import navBarScroll from './modules/btn-mobile.js';
-import initTabNav from './modules/initTabnav.js';
 
 navBar();
 navBarScroll();
-initTabNav();
